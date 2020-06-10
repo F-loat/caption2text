603ed5a1d3549850d8b951ddabec674c75501a79
F-loat <chaimaoyuan@foxmail.com>
2020-06-10 19:21:50 +0800

    添加 pwa 配置

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,16 @@ module.exports = {
   productionSourceMap: false,
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pwa: {
+    themeColor: '#474a4f',
+    msTileColor: '#fff',
+    iconPaths: {
+      favicon32: './img/icons/favicon-32x32.png',
+      favicon16: './img/icons/favicon-16x16.png',
+      appleTouchIcon: './img/icons/apple-touch-icon-152x152.png',
+      maskIcon: './img/icons/safari-pinned-tab.svg',
+      msTileImage: './img/icons/msapplication-icon-144x144.png'
+    }
+  }
 }
