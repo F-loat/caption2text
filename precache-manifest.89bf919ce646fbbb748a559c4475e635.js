2d18d611dcca106a5d7722007e377f066ce0e43a
F-loat <chaimaoyuan@foxmail.com>
2020-06-10 10:58:02 +0000

    Deploying to gh-pages from  @ 8733225173bab71d28344dae48e8a44ce2eb8d6a 🚀

---
+++ precache-manifest.89bf919ce646fbbb748a559c4475e635.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60320e345a564659ab39",
+    "url": "/caption2text/css/app.56fecb66.css"
+  },
+  {
+    "revision": "4c1c75cc75bdf6ffb3eb",
+    "url": "/caption2text/css/chunk-vendors.31b1eb73.css"
+  },
+  {
+    "revision": "ad4185a70328100d5e6ab64278ecd5ff",
+    "url": "/caption2text/index.html"
+  },
+  {
+    "revision": "60320e345a564659ab39",
+    "url": "/caption2text/js/app.0347f4d1.js"
+  },
+  {
+    "revision": "4c1c75cc75bdf6ffb3eb",
+    "url": "/caption2text/js/chunk-vendors.1959a583.js"
+  },
+  {
+    "revision": "7f11758975a3a0c5c326febf50ad3131",
+    "url": "/caption2text/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/caption2text/robots.txt"
+  },
+  {
+    "revision": "372c909c0d14d383cc98e1f36fa24dc0",
+    "url": "/caption2text/template.docx"
+  }
+]);
