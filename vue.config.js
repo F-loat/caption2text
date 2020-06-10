62a54df2658c115ed87ccc42c56c1f6f1785e9f0
F-loat <chaimaoyuan@foxmail.com>
2020-06-11 00:12:56 +0800

    移除冗余配置

--- vue.config.js
+++ vue.config.js
@@ -6,13 +6,6 @@ module.exports = {
   ],
   pwa: {
     themeColor: '#474a4f',
-    msTileColor: '#fff',
-    iconPaths: {
-      favicon32: './img/icons/favicon-32x32.png',
-      favicon16: './img/icons/favicon-16x16.png',
-      appleTouchIcon: './img/icons/apple-touch-icon-152x152.png',
-      maskIcon: './img/icons/safari-pinned-tab.svg',
-      msTileImage: './img/icons/msapplication-icon-144x144.png'
-    }
+    msTileColor: '#fff'
   }
 }
